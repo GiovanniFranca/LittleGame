75b6174fcdb49e85d92eed9b699809201afa2b3a
Giovanni Fernandes <giovanniffranca@hotmail.com>
2022-04-28 17:11:18 -0300

    fixed aim issue

--- game.js
+++ game.js
@@ -8,6 +8,10 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+let raf;
+const background = new Image();
+background.src = "https://wallpaperaccess.com/full/869.jpg";
+
 const gravity = 0.45;
 
 const menu = document.querySelector(".shop");
@@ -17,6 +21,136 @@ const cards = document.querySelectorAll(".card");
 const dmgPrice = document.querySelector(".dmg-price");
 const cdrPrice = document.querySelector(".cdr-price");
 const vlcPrice = document.querySelector(".vlc-price");
+
+let gameover = false;
+let spawnSpeed = 2000;
+let radius;
+let enemyHealth;
+let enemyDamage;
+
+let close;
+let damagePrice;
+let cooldownPrice;
+let velocityPrice;
+
+const health = document.querySelector(".health");
+
+const keyPressed = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+};
+let lastKeyPressed;
+
+const particles = [];
+const projectiles = [];
+const enemies = [];
+
+/////////////////////////////////////////
+// FUNCTIONS
+
+const randomColor = function () {
+  const randomR = Math.trunc(Math.random() * 255) + 1;
+  const randomG = Math.trunc(Math.random() * 255) + 1;
+  const randomB = Math.trunc(Math.random() * 255) + 1;
+
+  return `rgb(${randomR}, ${randomG}, ${randomB})`;
+};
+
+const detectCollisionWithPlayer = function (enemy, index) {
+  if (
+    enemy.position.y + enemy.radius >= player.position.y &&
+    enemy.position.x + enemy.radius >= player.position.x &&
+    enemy.position.x - enemy.radius < player.position.x + player.size.width
+  ) {
+    enemies.splice(index, 1);
+    player.health -= enemy.damage;
+    health.style.width = `${player.health}px`;
+  }
+  if (player.health <= 0) {
+    enemies.length = 0;
+    gameover = true;
+    window.cancelAnimationFrame(raf);
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "r" && gameover) init();
+    });
+  }
+};
+
+const detectCollision = function (enemy, index) {
+  if (
+    enemy.position.x + enemy.radius < 0 ||
+    enemy.position.x + enemy.radius > canvas.width ||
+    enemy.position.y - enemy.radius * 2 > canvas.height
+  ) {
+    setTimeout(() => {
+      enemies.splice(index, 1);
+    }, 0);
+  }
+};
+
+const detectProjectileCollision = function (projectile, enemy, index, indexP) {
+  const distance = Math.hypot(
+    projectile.position.x - enemy.position.x,
+    projectile.position.y - enemy.position.y
+  );
+  if (distance - projectile.radius - enemy.radius < 1) {
+    enemy.health -= player.damage;
+    setTimeout(() => {
+      projectiles.splice(indexP, 1);
+    }, 0);
+    if (enemy.health <= 0) {
+      setTimeout(() => {
+        enemies.splice(index, 1);
+        player.gold += 15;
+        player.health += 10;
+        health.style.width = `${player.health}px`;
+        gold.textContent = `${player.gold}G`;
+
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              { x: projectile.position.x, y: projectile.position.y },
+              Math.random() * 4,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 8),
+                y: (Math.random() - 0.5) * (Math.random() * 8),
+              }
+            )
+          );
+        }
+      }, 0);
+    }
+  }
+};
+const throttle = (func, limit) => {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
+
 // Inicia e configura todas as propriedades relativa ao player
 class Player {
   // prettier-ignore
@@ -56,13 +190,7 @@ class Player {
     }
   }
 }
-const randomColor = function () {
-  const randomR = Math.trunc(Math.random() * 255) + 1;
-  const randomG = Math.trunc(Math.random() * 255) + 1;
-  const randomB = Math.trunc(Math.random() * 255) + 1;
 
-  return `rgb(${randomR}, ${randomG}, ${randomB})`;
-};
 class Enemy {
   constructor(position, velocity, radius, health, damage, color) {
     this.position = position;
@@ -144,7 +272,7 @@ class Projectile {
 const player = new Player(
   { x: 100, y: 100 },
   { x: 0, y: 2 },
-  { width: 50, height: 150 },
+  { width: 50, height: 50 },
   { isOnGround: false },
   10,
   300,
@@ -152,34 +280,13 @@ const player = new Player(
   0.5,
   0
 );
-const throttle = (func, limit) => {
-  let lastFunc;
-  let lastRan;
-  return function () {
-    const context = this;
-    const args = arguments;
-    if (!lastRan) {
-      func.apply(context, args);
-      lastRan = Date.now();
-    } else {
-      clearTimeout(lastFunc);
-      lastFunc = setTimeout(function () {
-        if (Date.now() - lastRan >= limit) {
-          func.apply(context, args);
-          lastRan = Date.now();
-        }
-      }, limit - (Date.now() - lastRan));
-    }
-  };
-};
-const projectiles = [];
 
 const createProjectile = function (e) {
   const angle = Math.atan2(
     e.clientY - player.position.y,
-    e.clientX - player.position.x + player.size.width / 2
+    e.clientX - player.position.x - player.size.width * 5.6
   );
-  console.log(angle, Math.cos(angle), Math.sin(angle));
+  console.log(e.clientX - player.position.x);
   projectiles.push(
     new Projectile(
       {
@@ -187,26 +294,15 @@ const createProjectile = function (e) {
         y: player.position.y,
       },
       {
-        x: Math.cos(angle) * 9 * player.projectileVelocity,
-        y: Math.sin(angle) * 16 * player.projectileVelocity,
+        x: Math.cos(angle) * 5 * player.projectileVelocity,
+        y: Math.sin(angle) * 5 * player.projectileVelocity,
       },
       7
     )
   );
 };
 
-canvas.addEventListener(
-  "click",
-  throttle(createProjectile, player.cooldown),
-  true
-);
-const enemies = [];
-let spawnSpeed = 2000;
-let radius = 0;
-let enemyHealth = 30;
-let enemyDamage = 8;
-
-setInterval(() => {
+const spawnEnemies = () => {
   const x = Math.trunc(Math.random() * canvas.width);
   const y = -100;
   // prettier-ignore
@@ -217,7 +313,7 @@ setInterval(() => {
   radius = Math.trunc(Math.random() * 40) + 20;
   enemyDamage *= 1.01;
   enemyHealth *= 1.01;
-  spawnSpeed *= 0.99;
+  spawnSpeed *= 0.999;
   enemies.push(
     new Enemy(
       { x: x, y: y },
@@ -238,120 +334,12 @@ setInterval(() => {
       e.velocity.x = Math.cos(angle) * 4;
     });
   }, 800);
-}, spawnSpeed);
-
-const keyPressed = {
-  a: {
-    pressed: false,
-  },
-  d: {
-    pressed: false,
-  },
-  w: {
-    pressed: false,
-  },
-};
-let lastKeyPressed;
-
-document.addEventListener("keydown", function (e) {
-  switch (e.key) {
-    case "d":
-      keyPressed.d.pressed = true;
-      lastKeyPressed = "d";
-      return;
-    case "a":
-      keyPressed.a.pressed = true;
-      lastKeyPressed = "a";
-      return;
-    case "w":
-      if (player.isOnGround) {
-        player.velocity.y = -10;
-        return;
-      }
-  }
-});
-
-document.addEventListener("keyup", function (e) {
-  switch (e.key) {
-    case "d":
-      keyPressed.d.pressed = false;
-      return;
-    case "a":
-      keyPressed.a.pressed = false;
-      return;
-  }
-});
-const detectCollision = function (enemy, index) {
-  if (
-    enemy.position.x + enemy.radius * 2 < 0 ||
-    enemy.position.x + enemy.radius * 2 > canvas.width ||
-    enemy.position.y - enemy.radius * 2 > canvas.height
-  ) {
-    setTimeout(() => {
-      enemies.splice(index, 1);
-    }, 0);
-  }
-};
-const particles = [];
-const detectProjectileCollision = function (projectile, enemy, index, indexP) {
-  const distance = Math.hypot(
-    projectile.position.x - enemy.position.x,
-    projectile.position.y - enemy.position.y
-  );
-  if (distance - projectile.radius - enemy.radius < 1) {
-    enemy.health -= player.damage;
-    setTimeout(() => {
-      projectiles.splice(indexP, 1);
-    }, 0);
-    if (enemy.health <= 0) {
-      setTimeout(() => {
-        enemies.splice(index, 1);
-        player.gold += 30;
-        player.health += 10;
-        health.style.width = `${player.health}px`;
-        gold.textContent = `${player.gold}G`;
-
-        for (let i = 0; i < enemy.radius * 2; i++) {
-          particles.push(
-            new Particle(
-              { x: projectile.position.x, y: projectile.position.y },
-              Math.random() * 4,
-              enemy.color,
-              {
-                x: (Math.random() - 0.5) * (Math.random() * 8),
-                y: (Math.random() - 0.5) * (Math.random() * 8),
-              }
-            )
-          );
-        }
-      }, 0);
-    }
-  }
-};
-const health = document.querySelector(".health");
-health.style.width = `${player.health}px`;
-const detectCollisionWithPlayer = function (enemy, index) {
-  if (
-    enemy.position.y >= player.position.y &&
-    enemy.position.x >= player.position.x &&
-    enemy.position.x < player.position.x + player.size.width
-  ) {
-    enemies.splice(index, 1);
-    player.health -= enemy.damage;
-    health.style.width = `${player.health}px`;
-  }
-  if (player.health <= 0) {
-    window.cancelAnimationFrame(raf);
-  }
 };
 
-let raf;
-const background = new Image();
-background.src =
-  "https://img.wallpapersafari.com/desktop/1024/576/36/19/KODTtH.jpg";
 function animate() {
   raf = window.requestAnimationFrame(animate);
   c.drawImage(background, 0, 0, canvas.width, canvas.height);
+
   player.velocity.x = 0;
   if (keyPressed.d.pressed && lastKeyPressed === "d") {
     player.velocity.x = 5;
@@ -364,6 +352,7 @@ function animate() {
   if (player.position.x + player.size.width > canvas.width) {
     player.velocity.x = -1;
   }
+
   player.update();
   projectiles.forEach((p, index) => p.update(p, index));
   enemies.forEach((enemy, index) => {
@@ -382,50 +371,118 @@ function animate() {
     }
   });
 }
-animate();
-let close = true;
-let damagePrice = 100;
-let cooldownPrice = 100;
-let velocityPrice = 100;
-
-window.addEventListener("keydown", function (e) {
-  if (e.key === "b") {
-    menu.classList.toggle("hidden");
-    menu.addEventListener("click", function (e) {
-      if (e.target.closest(".card").classList.contains("damage")) {
-        if (+player.gold >= damagePrice) {
-          player.damage += 8;
-          player.gold -= damagePrice;
-          damagePrice += 50;
-          gold.textContent = player.gold;
-          dmgPrice.textContent = damagePrice;
+let spawn = setInterval(spawnEnemies, spawnSpeed);
+if (!gameover) {
+  window.addEventListener("keydown", function (e) {
+    if (e.key === (gameover ? "" : "b")) {
+      menu.classList.toggle("hidden");
+      menu.addEventListener("click", function (e) {
+        if (e.target.closest(".card").classList.contains("damage")) {
+          if (+player.gold >= damagePrice) {
+            player.damage += 8;
+            player.gold -= damagePrice;
+            damagePrice += 50;
+            gold.textContent = player.gold;
+            dmgPrice.textContent = damagePrice;
+          }
         }
-      }
-      if (e.target.closest(".card").classList.contains("cooldown")) {
-        if (+player.gold >= cooldownPrice) {
-          player.cooldown -= 60;
-          player.gold -= cooldownPrice;
-          gold.textContent = player.gold;
-          cooldownPrice += 50;
-          cdrPrice.textContent = cooldownPrice;
+        if (e.target.closest(".card").classList.contains("cooldown")) {
+          if (+player.gold >= cooldownPrice) {
+            player.cooldown -= 60;
+            player.gold -= cooldownPrice;
+            gold.textContent = player.gold;
+            cooldownPrice += 50;
+            cdrPrice.textContent = cooldownPrice;
+          }
         }
-      }
-      if (e.target.closest(".card").classList.contains("velocity")) {
-        if (+player.gold >= velocityPrice) {
-          player.projectileVelocity += 0.1;
-          player.gold -= velocityPrice;
-          gold.textContent = player.gold;
-          velocityPrice += 50;
-          vlcPrice.textContent = velocityPrice;
+        if (e.target.closest(".card").classList.contains("velocity")) {
+          if (+player.gold >= velocityPrice) {
+            player.projectileVelocity += 0.1;
+            player.gold -= velocityPrice;
+            gold.textContent = player.gold;
+            velocityPrice += 50;
+            vlcPrice.textContent = velocityPrice;
+          }
         }
+      });
+      if (close) {
+        this.window.cancelAnimationFrame(raf);
+        this.clearInterval(spawn);
+        close = !close;
+      } else {
+        animate();
+        close = !close;
+        spawn = setInterval(spawnEnemies, spawnSpeed);
       }
-    });
-    if (close) {
-      this.window.cancelAnimationFrame(raf);
-      close = !close;
-    } else {
-      animate();
-      close = !close;
     }
-  }
-});
+  });
+  document.addEventListener("keydown", function (e) {
+    switch (e.key) {
+      case "d":
+        keyPressed.d.pressed = true;
+        lastKeyPressed = "d";
+        return;
+      case "a":
+        keyPressed.a.pressed = true;
+        lastKeyPressed = "a";
+        return;
+      case "w":
+        if (player.isOnGround) {
+          player.velocity.y = -10;
+          return;
+        }
+    }
+  });
+
+  document.addEventListener("keyup", function (e) {
+    switch (e.key) {
+      case "d":
+        keyPressed.d.pressed = false;
+        return;
+      case "a":
+        keyPressed.a.pressed = false;
+        return;
+    }
+  });
+  canvas.addEventListener(
+    "click",
+    throttle(createProjectile, player.cooldown),
+    true
+  );
+}
+const init = function () {
+  gameover = false;
+
+  particles.length = 0;
+  projectiles.length = 0;
+  enemies.length = 0;
+
+  keyPressed.a.pressed = false;
+  keyPressed.d.pressed = false;
+  keyPressed.w.pressed = false;
+
+  spawnSpeed = 2000;
+  enemyHealth = 30;
+  enemyDamage = 8;
+  close = true;
+
+  damagePrice = 100;
+  cooldownPrice = 100;
+  velocityPrice = 100;
+
+  player.position.x = 100;
+  player.position.y = 100;
+  player.velocity.x = 0;
+  player.velocity.y = 2;
+  player.size.width = 50;
+  player.size.height = 50;
+  player.damage = 10;
+  player.health = 200;
+  player.cooldown = 600;
+  player.projectileVelocity = 0.5;
+  player.gold = 0;
+  health.style.width = `${player.health}px`;
+
+  animate();
+};
+init();
